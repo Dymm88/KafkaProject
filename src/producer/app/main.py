b31d95b3b4a4d9a371ba89bf0f769467c432641a
Dmitriy <ulrht88@gmail.com>
2024-10-14 09:39:00 +0300

    fix: remove created topic

--- src/producer/app/main.py
+++ src/producer/app/main.py
@@ -2,7 +2,6 @@ import json
 import os
 
 from confluent_kafka import Producer
-from confluent_kafka.admin import AdminClient, NewTopic
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -22,29 +21,9 @@ class Message(BaseModel):
 
 @app.post("/publish/")
 def publish_message(message: Message):
-    create_topic_if_not_exists('test_topic')
     producer.produce('test_topic', key='key', value=json.dumps(message.model_dump()), callback=delivery_report)
     producer.flush()
     return {"status": "Message sent", "message": message.text}
-
-
-def create_topic_if_not_exists(topic_name):
-    admin_client = AdminClient({'bootstrap.servers': os.getenv("KAFKA_ADVERTISED_LISTENERS", "kafka:9093")})
-    
-    topic_list = admin_client.list_topics().topics
-    
-    if topic_name not in topic_list:
-        new_topic = NewTopic(topic_name, num_partitions=1, replication_factor=1)
-        fs = admin_client.create_topics([new_topic])
-        
-        for topic, f in fs.items():
-            try:
-                f.result()
-                print(f"Топик '{topic}' создан.")
-            except Exception as e:
-                print(f"Ошибка при создании топика '{topic}': {e}")
-    else:
-        print(f"Топик '{topic_name}' уже существует.")
 
 
 def delivery_report(err, msg):
